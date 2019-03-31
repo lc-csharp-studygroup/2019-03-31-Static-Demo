a576cd798d233309db6037a36aa904519e8d3725
Sean Twiehaus <sean.twie03@gmail.com>
2019-03-31 17:46:35 -0500

    Add Broken Static Method as example

--- StaticDemo/StaticDemo/BankAccount.cs
+++ StaticDemo/StaticDemo/BankAccount.cs
@@ -7,5 +7,15 @@ namespace StaticDemo
     class BankAccount
     {
         public static decimal InterestRate { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public static void Withdrawal(decimal amount)
+        {
+            if (Balance > amount)
+            {
+                Balance - amount; 
+            }
+        }
     }
 }
