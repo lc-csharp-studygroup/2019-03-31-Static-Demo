cebc26af6cb11782366aa2c0dd5320a1446fbca3
Sean Twiehaus <sean.twie03@gmail.com>
2019-03-31 17:44:37 -0500

    Add BankAccount with Static Property

---
+++ StaticDemo/StaticDemo/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDemo
+{
+    class BankAccount
+    {
+        public static decimal InterestRate { get; set; }
+    }
+}
